f93d9b6d97f7e9a1b324116efdcb86386276f87f
amanvkalaskar <aman.kalaskar@spit.ac.in>
2021-02-27 16:23:09 +0530

    implementation

--- 3_Implementation/PythonCode/MarksAnalyzer.py
+++ 3_Implementation/PythonCode/MarksAnalyzer.py
@@ -1,31 +1,110 @@
-import random
-for i in range(25):
-  print(random.randint(40,100))
-# import plotly.graph_objects as go
+# import random
+# for i in range(25):
+#   print(random.randint(40,100))
 
-# def display(data):
-#   fig = go.Figure(data=go.Scatterpolar(
-#     r=data,
-#     theta=['processing cost','mechanical properties','chemical stability', 'thermal stability',
-#             'device integration'],
-#     fill='toself'
-#   ))
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+import csv
+import os
 
-#   fig.update_layout(
-#     polar=dict(
-#       radialaxis=dict(
-#         visible=True
-#       ),
-#     ),
-#     showlegend=False
-#   )
-#   fig.write_image("temp.png")
-
-# data=[5,2,3,4,5]
+def display(id,data):
+  fig = go.Figure(data=go.Scatterpolar(
+    r=data,
+    theta=['Computer','Physics','Chemistry', 'Biology',
+            'Maths'],
+    fill='toself'
+  ))
+  fig.update_layout(
+    polar=dict(
+      radialaxis=dict(
+        visible=True
+      ),
+    ),
+    showlegend=False
+  )
+  fig.write_image("results/"+str(id)+".png")
+data=[5,2,3,4,5]
 # display(data)
 
+#create a file for results
+if not os.path.exists('results'):
+    os.makedirs('results')
+
+
+ComputerMarks = open('dataset/Computer.csv', 'r')
+
+# Create a dataframe from csv
+df = pd.read_csv('dataset/Computer.csv',)
+# User list comprehension to create a list of lists from Dataframe rows
+list_of_rows = [list(row) for row in df.values]
+# print(df)
+f = open("results/studentMarks.csv", "w")
+f.write("studentID,studentNAME,studentEMAIL,Computer,Physics,Chemistry,Biology,Maths,Average\n")
+for i in list_of_rows:
+  # print(i)
+  for j in i:
+    # print(j)
+    f.write(str(j).strip()+",")
+
+  subPhy = pd.read_csv('dataset/Physics.csv', delimiter=',')  
+  if i[0] in subPhy.values:
+    temp_rows= [list(row) for row in subPhy.values]
+    for row in temp_rows:
+      if i[0] in row:
+        t=temp_rows.index(row)
+        break
+    f.write(str(subPhy.values[t][3])+",")
+  else: 
+    f.write("NA,")
+
+  subChe = pd.read_csv('dataset\Chemisty.csv', delimiter=',')  
+  if i[0] in subChe.values:
+    temp_rows= [list(row) for row in subChe.values]
+    for row in temp_rows:
+      if i[0] in row:
+        t=temp_rows.index(row)
+        break
+    f.write(str(subChe.values[t][3])+",")
+  else:
+    f.write("NA,")
+
+  subBio = pd.read_csv('dataset/Biology.csv', delimiter=',')  
+  if i[0] in subBio.values:
+    temp_rows= [list(row) for row in subBio.values]
+    for row in temp_rows:
+      if i[0] in row:
+        t=temp_rows.index(row)
+        break
+    f.write(str(subBio.values[t][3])+",")
+  else:
+    f.write("NA,")
+  
+  subMat = pd.read_csv('dataset/Maths.csv', delimiter=',')  
+  if i[0] in subMat.values:
+    temp_rows= [list(row) for row in subMat.values]
+    for row in temp_rows:
+      if i[0] in row:
+        t=temp_rows.index(row)
+        break
+    f.write(str(subMat.values[t][3])+",")
+  else:
+    f.write("NA,")
+
+  f.write("\n")
+f.close()
 
 
 
-
-
+data = pd.read_csv('results/studentMarks.csv', index_col=False)
+final = pd.DataFrame(data)
+print(final)
+# print(final["Physics"].mean())
+print(final.iloc[0,3:7].mean())
+print(final.iloc[0,0])
+# print(final.shape[0])
+display(final.iloc[0,0],final.iloc[0,3:7])
+for i in range(final.shape[0]):
+  display(final.iloc[i,0],final.iloc[i,3:7])
+# df["Average"] = final.iloc[0,3:final.shape[1]].mean()
+# print(df)
---
+++ 3_Implementation/PythonCode/dataset/random.py
@@ -0,0 +1,3 @@
+import random
+for i in range(25):
+  print(random.randint(40,100))
