ff41de72f8ac430933351dd777285b27a17ec950
amanvkalaskar <aman.kalaskar@spit.ac.in>
2021-02-27 09:27:00 +0530

    dataset

--- 3_Implementation/PythonCode/MarksAnalyzer.py
+++ 3_Implementation/PythonCode/MarksAnalyzer.py
@@ -1,28 +1,28 @@
-# import random
-# for i in range(10):
-#   print(random.randint(40,100))
-import plotly.graph_objects as go
+import random
+for i in range(25):
+  print(random.randint(40,100))
+# import plotly.graph_objects as go
 
-def display(data):
-  fig = go.Figure(data=go.Scatterpolar(
-    r=data,
-    theta=['processing cost','mechanical properties','chemical stability', 'thermal stability',
-            'device integration'],
-    fill='toself'
-  ))
+# def display(data):
+#   fig = go.Figure(data=go.Scatterpolar(
+#     r=data,
+#     theta=['processing cost','mechanical properties','chemical stability', 'thermal stability',
+#             'device integration'],
+#     fill='toself'
+#   ))
 
-  fig.update_layout(
-    polar=dict(
-      radialaxis=dict(
-        visible=True
-      ),
-    ),
-    showlegend=False
-  )
-  fig.write_image("temp.png")
+#   fig.update_layout(
+#     polar=dict(
+#       radialaxis=dict(
+#         visible=True
+#       ),
+#     ),
+#     showlegend=False
+#   )
+#   fig.write_image("temp.png")
 
-data=[5,2,3,4,5]
-display(data)
+# data=[5,2,3,4,5]
+# display(data)
 
 
 
