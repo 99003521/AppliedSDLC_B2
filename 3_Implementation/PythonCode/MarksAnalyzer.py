78348657d380847d0c810514dfe072efc451c3b2
amanvkalaskar <aman.kalaskar@spit.ac.in>
2021-02-27 08:59:35 +0530

    dataset

---
+++ 3_Implementation/PythonCode/MarksAnalyzer.py
@@ -0,0 +1,22 @@
+import plotly.graph_objects as go
+
+def display(data):
+  fig = go.Figure(data=go.Scatterpolar(
+    r=data,
+    theta=['processing cost','mechanical properties','chemical stability', 'thermal stability',
+            'device integration'],
+    fill='toself'
+  ))
+
+  fig.update_layout(
+    polar=dict(
+      radialaxis=dict(
+        visible=True
+      ),
+    ),
+    showlegend=False
+  )
+  fig.write_image("temp.png")
+
+data=[5,2,3,4,5]
+display(data)
