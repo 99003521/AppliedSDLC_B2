17fda5ae3e8aabf34dcbac395fbd1ba9c8d0e213
amanvkalaskar <aman.kalaskar@spit.ac.in>
2021-03-01 01:49:40 +0530

    email

--- 3_Implementation/PythonCode/mail.py
+++ 3_Implementation/PythonCode/mail.py
@@ -18,3 +18,21 @@ li = ['ragulperi@gmail.com','amanvkalaskar@gmail.com']
 
 #[item for item in input("Enter Receiver Mail Address :- ").split()] this is used to take user input of receiver mail id
 # getting length of list
+
+length = len(li)
+# Iterating the index
+# same as 'for i in range(len(list))'
+# Here we iterate the loop and send msg one by one to the reciver
+
+for i in range(length):
+
+    X = li[i]
+    reciver_mail = X
+ print(reciver_mail)
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] =  reciver_mail             #  Pass Reciver Mail Address
+    message['Subject'] =  'Mail using python' #The subject line
+    mail_content = '''Hi,
+
+    I am your mark analyser. This mail contains the final marksheet and analysed data of the subject modules. PFA
