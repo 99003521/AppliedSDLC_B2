49cceca954e0e36b4be92e42a58062c419027ca6
amanvkalaskar <aman.kalaskar@spit.ac.in>
2021-03-01 01:37:52 +0530

    UPDATE5.0

--- 3_Implementation/PythonCode/MarksAnalyzer.py
+++ 3_Implementation/PythonCode/MarksAnalyzer.py
@@ -105,24 +105,3 @@ subStats["Total"] = list(final.loc[: , "Computer":"Python"].sum())
 print(subStats)
 subStats.to_csv('results/SubjectStats.csv')
 
-
-
-# import smtplib
-
-# sender = 'amanajay.kalaskar@ltts.com'
-# receivers = ['ragulkrish974@gamil.com']
-
-# message = """From: From Person <from@fromdomain.com>
-# To: To Person <to@todomain.com>
-# Subject: SMTP e-mail test
-
-# This is a test e-mail message.
-# """
-
-# try:
-#    smtpObj = smtplib.SMTP('localhost')
-#    smtpObj.sendmail(sender, receivers, message)         
-#    print "Successfully sent email"
-# except SMTPException:
-#    print "Error: unable to send email"
-
