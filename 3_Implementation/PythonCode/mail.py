29659ecec91960ab1891a3696f24ba47451a9ff0
amanvkalaskar <aman.kalaskar@spit.ac.in>
2021-03-01 01:48:23 +0530

    mail code

--- 3_Implementation/PythonCode/mail.py
+++ 3_Implementation/PythonCode/mail.py
@@ -1,18 +1,20 @@
-import smtplib  
-# creates SMTP session 
-s = smtplib.SMTP('ragulperi@gmail.com', 587) 
-  
-# start TLS for security 
-s.starttls() 
-  
-# Authentication 
-s.login("ragulperi@gmail.com", "Krishna0918$") 
-  
-# message to be sent 
-message = "Testing ksro bhaiiiii"
-  
-# sending the mail 
-s.sendmail("ragulperi@gmail.com", "jocago3836@mayhco.com", message) 
-  
-# terminating the session 
-s.quit() 
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+
+from email.mime.text import MIMEText
+
+from email.mime.base import MIMEBase
+
+from email import encoders
+#The mail addresses and  password
+
+sender_address = 'sdlcgen@gmail.com' #put ur mailid
+
+sender_pass = 'SDLCGEN1234' #ur mailpass
+# list of reciver email_id to the mail
+
+li = ['ragulperi@gmail.com','amanvkalaskar@gmail.com']
+
+#[item for item in input("Enter Receiver Mail Address :- ").split()] this is used to take user input of receiver mail id
+# getting length of list
