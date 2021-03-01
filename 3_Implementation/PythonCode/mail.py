4c48ed3d586d1f20ae9a7339384f91b839a02fe2
GraceJoseph7 <79656565+GraceJoseph7@users.noreply.github.com>
2021-03-01 07:10:01 +0530

    Update mail.py

--- 3_Implementation/PythonCode/mail.py
+++ 3_Implementation/PythonCode/mail.py
@@ -14,7 +14,7 @@ sender_address = 'sdlcgen@gmail.com' #put ur mailid
 sender_pass = 'SDLCGEN1234' #ur mailpass
 # list of reciver email_id to the mail
 
-li = ['ragulperi@gmail.com','amanvkalaskar@gmail.com']
+li = ['ragulperi@gmail.com','amanvkalaskar@gmail.com','gracej755@gmail.com']
 
 #[item for item in input("Enter Receiver Mail Address :- ").split()] this is used to take user input of receiver mail id
 # getting length of list
